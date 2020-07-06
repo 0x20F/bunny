d5f4ef0d6edc985c520f3d0f9259580a8ec02a19
0x20F <alexhexan22@gmail.com>
2020-07-06 08:13:08 +0200

    Barebones and ugly engine to parse config file and find the right bookmark

--- src/books/mod.rs
+++ src/books/mod.rs
@@ -4,19 +4,13 @@ mod github;
 
 use std::fs;
 use toml::Value;
-
+use toml::value::Table;
+use key_list::KeyList;
 
 
 pub fn open_book(query: &str) -> String {
     let (command, params) = command_from_query(&query);
-
-    resolve_book_url(command, params);
-
-    match command {
-        "gh" => github::construct_github_url(params),
-        "tw" => twitter::construct_twitter_url(params),
-        _ => google::construct_google_search_url(params)
-    }
+    resolve_book_url(command, params)
 }
 
 
@@ -35,42 +29,102 @@ fn command_from_query(query: &str) -> (&str, &str) {
 }
 
 
-fn resolve_book_url(command: &str, params: &str) {
+fn value_as_str<'a>(value: &'a Value, key: &str) -> &'a str {
+    match value.get(key) {
+        Some(v) => v.as_str().unwrap(),
+        None => ""
+    }
+}
+
+
+fn value_as_table<'a>(value: &'a Value, key: &str) -> &'a Table {
+    value.get(key)
+        .unwrap()
+        .as_table()
+        .unwrap()
+}
+
+
+fn replace_keys(text: &str, data: &str) -> String {
+    let keys = KeyList::new(text, '{', '}');
+    let mut clean = String::from(text);
+
+    for key in keys {
+        match key {
+            "{encoded}" => clean = text.replace(
+                key,
+                crate::encoder::encode(data).as_ref()
+            ),
+            "{raw}" => clean = text.replace(key, data),
+            _ => ()
+        }
+    }
+
+    clean
+}
+
+
+fn search_engine_query(data: &str) -> String {
+    // google, for now
+    let encoded = crate::encoder::encode(data);
+    format!("https://google.com/search?q={}", encoded)
+}
+
+
+fn resolve_book_url(command: &str, params: &str) -> String {
     let contents = fs::read_to_string("books.toml")
         .expect("Something went wrong when reading the file");
 
     let config = contents.parse::<Value>().unwrap();
-    let table = config.as_table().unwrap();
+    let books = config.as_table().unwrap();
 
-    for (_, value) in table.iter() {
-        let alias = value.get("alias")
-            .unwrap()
-            .as_str()
-            .unwrap();
 
-        if command != alias {
+    for (_, book) in books.iter() {
+        let alias = value_as_str(book, "alias");
+
+        if command == alias {
+            let url = resolve_correct_option(book, params);
+
+            if let Some(url) = url {
+                return url;
+            }
+        }
+    }
+
+    // If nothing was found, go to a search engine
+    search_engine_query(format!("{} {}", command, params).as_ref())
+}
+
+
+fn resolve_correct_option(book: &Value, params: &str) -> Option<String> {
+    let options = value_as_table(book, "options");
+    let mut default: Option<&Value> = None;
+
+    for (name, option) in options.iter() {
+        if name == "default" {
+            default = Some(option);
             continue;
         }
 
-        println!("Command '{}' was given and found", command);
+        let prefix = value_as_str(option, "prefix");
+        let url = value_as_str(option, "url");
 
-        let books = value.get("books")
-            .unwrap()
-            .as_table()
-            .unwrap();
+        if params.starts_with(prefix) {
+            let params = params.replace(prefix, "");
+            let url = replace_keys(url, params.trim());
 
-        for (book, value) in books.iter() {
-            let format = value.get("format").unwrap();
-            let url = value.get("url").unwrap();
-
-            println!(
-                "Gotta get the format '{}' to somehow check validity of passed in params '{}'",
-                format,
-                params
-            );
+            return Some(url);
         }
     }
+
+    match default {
+        Some(v) => Some(value_as_str(v, "url").to_owned()),
+        None => None
+    }
 }
+
+
+
 
 
 
